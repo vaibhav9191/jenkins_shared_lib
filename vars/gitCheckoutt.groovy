5fbaa4354e35ab4f571eee7daffe057e2b1d3757
vaibhav <ccc@dddd.com>
2023-06-06 12:28:06 +0530

    vv

--- vars/gitCheckoutt.groovy
+++ vars/gitCheckoutt.groovy
@@ -1,11 +1,11 @@
-// def call(Map stageParams) {
+def call(Map stageParams) {
  
-//     checkout([
-//         $class: 'GitSCM',
-//         branches: [[name:  stageParams.branch ]],
-//         userRemoteConfigs: [[ url: stageParams.url ]]
-//     ])
-//   }
+    checkout([
+        $class: 'GitSCM',
+        branches: [[name:  stageParams.branch ]],
+        userRemoteConfigs: [[ url: stageParams.url ]]
+    ])
+  }
 
 // def call(){
 
